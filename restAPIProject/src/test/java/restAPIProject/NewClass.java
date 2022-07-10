5ea6faa3acc82d1b37b761821aad0ea7e8961e10
AlexandrZ0 <maibah77@gmail.com>
2022-07-11 00:33:33 +0300

    И снова здравствуйте !!!

--- restAPIProject/src/test/java/restAPIProject/NewClass.java
+++ restAPIProject/src/test/java/restAPIProject/NewClass.java
@@ -3,7 +3,8 @@ package restAPIProject;
 public class NewClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("I'm the new class !!!");
+		System.out.println("И снова здравствуйте ....");
 
 	}
 
