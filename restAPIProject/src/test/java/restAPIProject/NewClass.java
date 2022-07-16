33be043c7fe06791e4dc46574c7d6f1351d3984b
AlexandrZ0 <maibah77@gmail.com>
2022-07-16 16:40:14 +0300

    change method was added

--- restAPIProject/src/test/java/restAPIProject/NewClass.java
+++ restAPIProject/src/test/java/restAPIProject/NewClass.java
@@ -5,6 +5,7 @@ public class NewClass {
 	public static void main(String[] args) {
 		NewClass newClass = new NewClass();
 		newClass.going();
+		newClass.changeVal(8);
         
 	}
 	
@@ -13,6 +14,9 @@ public class NewClass {
 		System.out.println("I am going");
 	}
 	
+	int changeVal(int val) {
+		return val-2;
+	}
 	
 
 }
